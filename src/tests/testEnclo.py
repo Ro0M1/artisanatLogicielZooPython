a4a21c5f392cd53c8618af4a05f4dfd194ee05c2
Ro0M1 <95227636+Ro0M1@users.noreply.github.com>
2024-06-19 18:49:50 +0200

    Correction.

--- src/tests/testEnclo.py
+++ src/tests/testEnclo.py
@@ -3,17 +3,17 @@ import unittest
 
 class TestAnimal(unittest.TestCase):
 
-    def test_On_peut_récupérer_un_enclos_du_systeme(self):
+    def test_On_peut_récupérer_un_enclo_du_systeme(self):
         pass
 
-    def test_On_peut_ajouter_un_enclos_du_systeme(self):
+    def test_On_peut_ajouter_un_enclo_du_systeme(self):
         pass
 
-    def test_On_peut_modifier_un_enclos_du_systeme(self):
+    def test_On_peut_modifier_un_enclo_du_systeme(self):
         pass
 
-    def test_On_peut_supprimer_un_enclos_vide_du_systeme(self):
+    def test_On_peut_supprimer_un_enclo_vide_du_systeme(self):
         pass
 
-    def test_On_ne_peut_pas_supprimer_un_enclos_non_vide_du_systeme(self):
+    def test_On_ne_peut_pas_supprimer_un_enclo_non_vide_du_systeme(self):
         pass
